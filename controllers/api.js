23c2218a7603e82d20c895ae83001e7defdffc22
URIZ <uriz@il.ibm.com>
2018-02-04 13:58:18 +0200

    basic impl for create user

--- controllers/api.js
+++ controllers/api.js
@@ -1,5 +1,6 @@
 'use strict';
 let passport = require('passport');
+let request = require('request-promise');
 
 module.exports.getUser = function getUser (req, res, next) {
 
@@ -10,7 +11,37 @@ module.exports.getUser = function getUser (req, res, next) {
 
 module.exports.createUser = function getUser (req, res, next) {
 
-     res.send("user created!!!!!!" + JSON.stringify(req.user));
+
+     if (req.user){
+
+
+
+         let options = {
+             uri: "https://mcn-user-service.herokuapp.com/api/users",
+             qs: {
+                 fb_user_id: req.user.id.value,
+                 fb_user_name: req.user.displayName.value,
+                 email:req.user.emails.value[0]
+             },
+             headers: {
+                 'User-Agent': 'Request-Promise'
+             },
+             json: true // Automatically parses the JSON string in the response
+         };
+
+         rp(options)
+             .then(function (result) {
+                 console.log(result);
+                 res.send(result)
+             })
+             .catch(function (err) {
+                 // API call failed...
+             });
+
+
+     }
+     else
+          res.send("error!!!!!!" + JSON.stringify(req.user));
 
 };
 
